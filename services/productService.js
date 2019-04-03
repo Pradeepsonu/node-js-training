f43f6c93f1760483925b0b7c1e26303561f78483
Pradeep <pradeepke28@gmail.com>
2019-04-03 10:09:36 +0530

    Pradeep | Initialization service 2.Manufacturer management

--- services/productService.js
+++ services/productService.js
@@ -1,46 +1,25 @@
-exports.getProducts = () => {
-    return [
-        {
-            "mobile": "F11 pro",
-            "comapany": "Oppo",
-            "manufacture": "India",
-            "owner": "Someone",
-            "price": "20,000"
-        },
-        {
-            "mobile": "One plus 7",
-            "comapany": "One plus",
-            "manufacture": "India",
-            "owner": "Someone",
-            "price": "40,000"
-        },
-        {
-            "mobile": "Canvas",
-            "comapany": "Micromax",
-            "manufacture": "China",
-            "owner": "New",
-            "price": "10,000"
-        },
-        {
-            "mobile": "Xs max",
-            "comapany": "Apple",
-            "manufacture": "USA",
-            "owner": "Someone",
-            "price": "80,000"
-        },
-        {
-            "mobile": "Zx",
-            "comapany": "Sony",
-            "manufacture": "China",
-            "owner": "Someone",
-            "price": "40,000"
-        },
-        {
-            "mobile": "V11 pro",
-            "comapany": "Vivo",
-            "manufacture": "China",
-            "owner": 'Someone',
-            "price": "20,000"
-        }
-    ]
+const client = require('../db')
+
+
+
+exports.getProducts = async () => {
+    const result = await client.query('select * from products')
+    return result.rows;
+}
+
+exports.getProductsById = async (id) => {
+    const result = await client.query(`select * from products where id = ${id}`)
+    return result.rows[0];
+}
+
+exports.insert = async (userID, manufacturerID, product) => {
+    try {
+        const query = 'insert into products (name, code,manufacturer_id, description, created_at,created_by,price,quantity) VALUES($1, $2, $3, $4, $5, $6, $7, $8) '
+        const values = [product.name, product.code, manufacturerID, product.description, new Date(), userID, product.price, product.quantity]
+        await client.query(query, values)
+    } catch (err) {
+        console.log(err)
+        throw 'Unable to insert product'
+    }
+
 }
--- services/userService.js
+++ services/userService.js
@@ -1,84 +1,23 @@
-exports.getUsers = () => {
-    return [
-        {
-            firstName: "Pradeep",
-            lastName: "Kumar",
-            email: "pradeepke28@gmail.com",
-            phone: "7358318949",
-            designation: "Trainee",
-            status: "Active"
-        },
-        {
-            firstName: "Pradeep",
-            lastName: "Kumar",
-            email: "pradeepke28@gmail.com",
-            phone: "7358318949",
-            designation: "Trainee",
-            status: "Inactive"
-        },
-        {
-            firstName: "Pradeep",
-            lastName: "Kumar",
-            email: "pradeepke28@gmail.com",
-            phone: "7358318949",
-            designation: "Trainee",
-            status: "Active"
-        },
-        {
-            firstName: "Pradeep",
-            lastName: "Kumar",
-            email: "pradeepke28@gmail.com",
-            phone: "7358318949",
-            designation: "Trainee",
-            status: "Inactive"
-        },
-        {
-            firstName: "Pradeep",
-            lastName: "Kumar",
-            email: "pradeepke28@gmail.com",
-            phone: "7358318949",
-            designation: "Trainee",
-            status: "Active"
-        },
-        {
-            firstName: "Pradeep",
-            lastName: "Kumar",
-            email: "pradeepke28@gmail.com",
-            phone: "7358318949",
-            designation: "Trainee",
-            status: "Inactive"
-        },
-        {
-            firstName: "Pradeep",
-            lastName: "Kumar",
-            email: "pradeepke28@gmail.com",
-            phone: "7358318949",
-            designation: "Trainee",
-            status: "Active"
-        },
-        {
-            firstName: "Pradeep",
-            lastName: "Kumar",
-            email: "pradeepke28@gmail.com",
-            phone: "7358318949",
-            designation: "Trainee",
-            status: "Inactive"
-        },
-        {
-            firstName: "Pradeep",
-            lastName: "Kumar",
-            email: "pradeepke28@gmail.com",
-            phone: "7358318949",
-            designation: "Trainee",
-            status: "Active"
-        },
-        {
-            firstName: "Pradeep",
-            lastName: "Kumar",
-            email: "pradeepke28@gmail.com",
-            phone: "7358318949",
-            designation: "Trainee",
-            status: "Inactive"
-        }
-    ]
+const client = require('../db')
+
+exports.getUsers = async () => {
+    const result = await client.query('select * from users')
+    return result.rows;
+}
+
+exports.getUsersById = async (id) => {
+    const result = await client.query(`select * from users where id = ${id}`)
+    return result.rows[0];
+}
+
+exports.insert = async (users) => {
+    try {
+        const query = 'insert into users (first_name,last_name, username, phone,email,status,designation,isdeleted) VALUES($1, $2, $3, $4, $5, $6, $7, $8) '
+        const values = [users.first_name, users.last_name, users.username, users.phone, users.email, users.status, users.designation, users.isdeleted]
+        await client.query(query, values)
+    } catch (err) {
+        console.log(err)
+        throw 'Unable to insert product'
+    }
+
 }
